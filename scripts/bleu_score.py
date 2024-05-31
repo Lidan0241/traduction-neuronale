751858e7d564cdfaed38063c67caa7a4b7d18865
Lidan Zhang <zhanglidan2002@gmail.com>
2024-05-31 21:09:18 +0200

    ajout pred combined

--- scripts/bleu_score.py
+++ scripts/bleu_score.py
@@ -1,21 +1,19 @@
-
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu, SmoothingFunction
 
-# Load the data from the uploaded files
-with open('../src/predictions/combined_lemme_pred_500.fr', 'r', encoding='utf-8') as f:
+with open('../src/predictions/combined_lemme_pred.fr', 'r', encoding='utf-8') as f:
     predictions = f.readlines()
 
-with open('../data/clean/combined_test_500_lemme.fr', 'r', encoding='utf-8') as f:
+with open('../data/clean/combined_test_lemme.fr', 'r', encoding='utf-8') as f:
     references = f.readlines()
 
-# Process data to remove leading/trailing whitespaces and split into list of lists
+
 predictions = [line.strip() for line in predictions]
 references = [[line.strip()] for line in references]  # BLEU expects list of references per hypothesis
 
 #print(len(predictions))
 #print(len(references))
 
-# Calculate BLEU score
+# calculer BLEU score
 smoothing_function = SmoothingFunction().method1
 bleu_score = corpus_bleu(references, predictions, smoothing_function=smoothing_function)
 
